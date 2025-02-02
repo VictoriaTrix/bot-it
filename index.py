5b2944c700d33513e5c6a8e82f2cca4ff2483ea6
Victoria Trix <vikaparusimova1234@gmail.com>
2025-02-02 16:33:59 +0500

    first commit

---
+++ index.py
@@ -0,0 +1,30 @@
+#    Задание 1
+# sum=0
+# a=[7,5,2,4,9]
+# for i in a:
+#     sum+=i
+# print(sum)
+    
+    #  Задание 2
+# a=[2,7,5,8,4,6]
+# for i in a:
+#     if i%2==0:
+#         print(i)
+
+
+#Задание 3
+# numbers=[2,3,5,6,1,4,8,7,9]
+# max_number=numbers[0]
+# for number in numbers:
+#     if number>max_number:
+#         max_number=number
+# print("Самое большое число:",max_number)
+
+#Задание 4
+# numbers = [1, 2, 5, 4, 6, 7, 9, 8]
+# for i in range(len(numbers) - 1, -1, -1):
+#     print(numbers[i])
+
+  #Задание 5      
+for i in range(1, 11):
+ print(5*i)
